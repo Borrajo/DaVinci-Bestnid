278cde014ae1b5135acc8820502035e7c23e52e8
Campagna <azurecuenta@gmail.com>
2014-11-07 18:44:51 -0300

    Modificacion 07/11

--- bestnid-grupo2/app/models/producto.rb
+++ bestnid-grupo2/app/models/producto.rb
@@ -5,6 +5,5 @@ class Producto < ActiveRecord::Base
 	belongs_to :usuarios
 
 	validates :nombre,presence: true,length: {minimum: 4, maximum: 30}
-
 	validates :descripcion, presence: true,length: {minimum: 10, maximum: 500}
 end
