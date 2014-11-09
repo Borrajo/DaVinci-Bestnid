efe141a82a129acf462a2313f0573b2da4d0054b
Borrajo <borrajo@hotmail.com.ar>
2014-11-09 11:30:57 -0300

    Pagina funcionando
    Agregue la pagina de productos, la pagina de cada producto, hice una
    migracion, y alguna cosa mas.

---
+++ bestnid-grupo2/app/controllers/productos_controller.rb
@@ -0,0 +1,17 @@
+class ProductosController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def show 
+  end
+end
---
+++ bestnid-grupo2/app/controllers/usuarios_controller.rb
@@ -0,0 +1,28 @@
+class UsuariosController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def buy
+  end
+
+  def sell
+  end
+
+  def ask
+  end
+
+  def answer
+  end
+
+  def show
+  end
+end
