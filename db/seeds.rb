eb10c763d106a2a23e3d6d6c9440751c4ef626a0
Borrajo <borrajo@hotmail.com.ar>
2014-11-13 13:31:46 -0300

    Arreglo de fechas en seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,16 +31,16 @@ Producto.find_or_create_by(nombre: "Guantes de acero", descripcion: "Guante de m
 	created_at: DateTime.new(2014,2,5), fecha_finalizacion: DateTime.new(2014,2,5) + 15.day)
 Producto.find_or_create_by(nombre: "Llama", descripcion: "Llama adulta oriunda de Tilcara. Es mansita", 
 	id_owner: 2, cantidad_preguntas: 0, categoria: 1, foto: "http://41.media.tumblr.com/6750bde49985d14340294b65cbfac7a2/tumblr_nbsedz4pML1tlipbuo1_1280.jpg",
-	created_at: DateTime.new(2014,3,6), fecha_finalizacion: DateTime.new(2014,2,5) + 20.day)
+	created_at: DateTime.new(2014,3,6), fecha_finalizacion: DateTime.new(2014,3,6) + 20.day)
 Producto.find_or_create_by(nombre: "Espejo", descripcion: "Espejo sin marco. Medidas: 0.8m x 1.2m", 
 	id_owner: 1, cantidad_preguntas: 0, categoria: 2, foto: "http://40.media.tumblr.com/ad056adadfeeced7f24f918a843a0f60/tumblr_nbsf4rGZFE1tlipbuo1_1280.jpg",
-	created_at: DateTime.new(2014,4,7), fecha_finalizacion: DateTime.new(2014,2,5) + 15.day)
+	created_at: DateTime.new(2014,4,7), fecha_finalizacion: DateTime.new(2014,4,7) + 15.day)
 Producto.find_or_create_by(nombre: "Kriptonita", descripcion: "200 gramos de Kriptonita", 
 	id_owner: 2, cantidad_preguntas: 0, categoria: 4, foto: "http://41.media.tumblr.com/8e08757ed8fd1c3368e29dc127140ef1/tumblr_nbsewqvU231tlipbuo1_1280.jpg",
-	created_at: DateTime.new(2014,5,8), fecha_finalizacion: DateTime.new(2014,2,5) + 28.day)
+	created_at: DateTime.new(2014,5,8), fecha_finalizacion: DateTime.new(2014,5,8) + 28.day)
 Producto.find_or_create_by(nombre: "Aceite y Vinagre", descripcion: "200ml de aceite y 300ml de vinagre. No incluye fascos", 
 	id_owner: 3, cantidad_preguntas: 0, categoria: 8, foto: "http://40.media.tumblr.com/f92d39b2c62cf2bc5398797db35ce37d/tumblr_nbsejfmSt21tlipbuo1_400.jpg",
-	created_at: DateTime.new(2014,6,9), fecha_finalizacion: DateTime.new(2014,2,5) + 15.day)
+	created_at: DateTime.new(2014,6,9), fecha_finalizacion: DateTime.new(2014,6,9) + 15.day)
 
 puts "Agrego una pregunta"
 Producto.find(2).preguntas.find_or_create_by(pregunta: "Cuando pesa?");
