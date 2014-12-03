21d275e360d18bd1a34505d57ab9187399f13b17
Campagna <azurecuenta@gmail.com>
2014-12-03 15:46:22 -0300

    Registro - Campos Text - Traduccion a Castellano

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up){  |u| u.permit(:nombre, :email, :password, :apellido, :telefono, :foto,:fecha_nacimiento, :domicilio, :numero_tarjeta, :titular_tarjeta, :vencimiento_tarjeta) };
+            devise_parameter_sanitizer.for(:sign_up){  |u| u.permit(:nombre, :email, :password, :password_confirmation, :apellido, :telefono, :foto,:fecha_nacimiento, :domicilio, :numero_tarjeta, :titular_tarjeta, :vencimiento_tarjeta) };
             
-            devise_parameter_sanitizer.for(:account_update){  |u| u.permit(:nombre, :email, :password, :apellido, :telefono, :foto, :fecha_nacimiento, :domicilio, :numero_tarjeta, :titular_tarjeta, :vencimiento_tarjeta) }
+            devise_parameter_sanitizer.for(:account_update){  |u| u.permit(:nombre, :email, :password, :password_confirmation, :apellido, :telefono, :foto, :fecha_nacimiento, :domicilio, :numero_tarjeta, :titular_tarjeta, :vencimiento_tarjeta) }
         end
 end
