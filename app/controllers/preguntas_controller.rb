55518110e4edf4d09fe7877fb53c70401ff5e299
facundodf <faku.f@live.com.ar>
2014-12-07 18:04:11 -0300

    progreso preg/resp

--- app/controllers/preguntas_controller.rb
+++ app/controllers/preguntas_controller.rb
@@ -4,12 +4,12 @@ class PreguntasController < ApplicationController
 
   def create
      @pregunta = Pregunta.new(pregunta_params)
-     if @pregunta.save
+      if @pregunta.save
       #no pueden usarse tildes en los flashes
       flash[:success] = "Tu pregunta fue realizada con exito"
       ActionCorreo.bienvenido_email(current_user).deliver
       redirect_to producto_path(params[:pregunta][:producto_id])
-   end
+      end
   end
 
   def destroy
@@ -22,11 +22,20 @@ class PreguntasController < ApplicationController
   end
 
   def edit
+     @pregunta = {respuesta_params}
+      if @pregunta.save
+      #no pueden usarse tildes en los flashes
+      flash[:success] = "Tu respuesta fue realizada con exito"
+      redirect_to edit_pregunta_path(@product)
+      end
     
   end
 
   private
   def pregunta_params
-    params.require(:pregunta).permit(:pregunta, :producto_id, :usuario_id, :respuesta, :fecha_respuesta) 
+    params.require(:pregunta).permit(:pregunta, :producto_id, :usuario_id) 
+  end
+  def respuesta_params
+    params.require(:respuesta).permit(:pregunta, :producto_id, :usuario_id, :respuesta, :fecha_respuesta) 
   end
 end
