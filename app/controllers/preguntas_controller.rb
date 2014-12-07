a46f06a22f97bf957aa68b7ae32f4ba3676b8548
Campagna <azurecuenta@gmail.com>
2014-12-07 20:20:04 -0300

    pregunta/respuesta

--- app/controllers/preguntas_controller.rb
+++ app/controllers/preguntas_controller.rb
@@ -22,18 +22,26 @@ class PreguntasController < ApplicationController
   end
 
   def edit
-     @pregunta = {respuesta_params}
-      if @pregunta.save
-      #no pueden usarse tildes en los flashes
-      flash[:success] = "Tu respuesta fue realizada con exito"
-      redirect_to edit_pregunta_path(@product)
+    @pregunta = Pregunta.find(params[:id])
+  end
+
+  def update
+     @pregunta = Pregunta.find(params[:id])
+      if @pregunta.update_attributes(pregunta_params)
+        #no pueden usarse tildes en los flashes
+        flash[:success] = "Tu respuesta fue realizada con exito"
+        redirect_to pregunta_path(params[:pregunta][:producto_id])
+      else
+        render 'edit'
+        #no pueden usarse tildes en los flashes
+        flash[:danger] = "No se pudo generar la respuesta y no sabemos porque :S"
+        redirect_to pregunta_path(params[:pregunta][:producto_id])
       end
-    
   end
 
   private
   def pregunta_params
-    params.require(:pregunta).permit(:pregunta, :producto_id, :usuario_id) 
+    params.require(:pregunta).permit(:pregunta, :producto_id, :usuario_id, :respuesta, :fecha_respuesta) 
   end
   def respuesta_params
     params.require(:respuesta).permit(:pregunta, :producto_id, :usuario_id, :respuesta, :fecha_respuesta) 
