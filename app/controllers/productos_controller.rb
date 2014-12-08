55d2d8054eca5532e4425101659cc70aed3ca1ed
Campagna <azurecuenta@gmail.com>
2014-12-08 11:28:34 -0300

    Contestar preguntas resuelto

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -38,8 +38,23 @@ class ProductosController < ApplicationController
   def destroy
   end
 
-def edit
-end
+  def edit
+    @pregunta = Pregunta.find(params[:id])
+  end
+
+  def update
+     @pregunta = Pregunta.find(params[:id])
+      if @pregunta.update_attributes(pregunta_params)
+        #no pueden usarse tildes en los flashes
+        flash[:success] = "Tu respuesta fue realizada con exito"
+        redirect_to producto_path
+      else
+        render 'edit'
+        #no pueden usarse tildes en los flashes
+        flash[:danger] = "No se pudo generar la respuesta y no sabemos porque :S"
+        redirect_to pregunta_path(params[:pregunta][:producto_id])
+      end
+  end
 
 #Aca esta lo qeu te decía  si logras asociar una view para este metodo ya estaríamos 
   def ver
@@ -60,6 +75,10 @@ end
   
   def producto_params
    params.require(:producto).permit(:nombre, :descripcion, :usuario_id, :categoria_id, :fecha_finalizacion, :foto) 
+  end
+  
+  def pregunta_params
+    params.require(:pregunta).permit(:pregunta, :producto_id, :usuario_id, :respuesta, :fecha_respuesta) 
   end
 
   def get_producto
--- app/models/pregunta.rb
+++ app/models/pregunta.rb
@@ -1,5 +1,7 @@
 class Pregunta < ActiveRecord::Base
 
+  default_scope -> {order("id")} 
+  
   	scope :no_contestadas, -> {where("respuesta IS NULL")} 
 
   belongs_to :productos, dependent: :destroy
